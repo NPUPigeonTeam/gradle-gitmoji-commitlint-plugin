7df275235f41f21f901ad6479bbbc8e8e15d15c3
LykorisR <lykorisr@proton.me>
2023-03-16 17:08:42 +0800

    🐛 fix: fix regex error

--- src/main/groovy/fun/lykorisr/commitlint/CommitlintUtil.groovy
+++ src/main/groovy/fun/lykorisr/commitlint/CommitlintUtil.groovy
@@ -64,7 +64,7 @@ class CommitlintUtil {
       .split("\n")
       .findAll { !it.trim().startsWith("#") }
     
-    def re = /^(?::\w*:|(?:(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])))\s(?<type>\w*)(?:\((?<scope>.*)\))?!?:\s(?<subject>(?:(?!#).)*(?:(?!\s).))(?:\s\(?(?<ticket>#\d*)\)?)?\u0024/
+    def re = /^(?::\w*:|(?:(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])))\s(?<type>\w*)(?:\((?<scope>.*)\))?!?:\s(?<subject>(?:(?!#).)*(?:(?!\s).))(?:\s\(?(?<ticket>#\d*)\)?)?$/
     if (!(lines[0] =~ re)) {
       throw new InvalidUserDataException(E_INVALID_TYPE)
     }
